8d6307fdfc7e6991f44720750eddb995d803b01a
LeahJKH <LeahJb06.kodehode@gmail.com>
2025-02-13 11:08:08 +0100

    fixed spelling mistakes and added new img feature

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
-const txtField = document.querySelector("#txt-field")
-const storyField = document.querySelector("#story")
-const nextD = document.querySelector("#nextD")
+const txtField = document.querySelector("#txt-field");
+const storyField = document.querySelector("#story");
+const nextD = document.querySelector("#nextD");
+const imageCont = document.querySelector("#image-cont")
 const intro = [
-    "so get this right", "ur in the forest", "what else?", "oh your name?"
-]
+    "So get this right...",
+    "You're in the forest.",
+    "What else?",
+    "Oh, your name?"
+];
 
-let intiger = 0
-txtField.addEventListener("change", (e) => {
-    console.log(txtField.value)
-    
-})
+let index = 0;
 
-storyField.innerHTML = `<p>${intro[intiger]}</p>`
-nextD.addEventListener("click", (e) => {
-intiger++
-storyField.innerHTML = `<p>${intro[intiger]}</p>`
-if (intiger == 3) {
-    intiger = 3
-    nextD.textContent = "submit"
-    nameOfChar = txtField.value
-    console.log(nameOfChar)
+storyField.innerHTML = `<p>${intro[index]}</p>`;
+
+nextD.addEventListener("click", () => {
+    index++;
+
+    if (index < intro.length) {
+        storyField.innerHTML = `<p>${intro[index]}</p>`;
+    }
+    if (index = 1) {
+        imageCont.style.backgroundImage = "url(./images/forest.png)"
+    }
+
+    if (index === intro.length - 1) {
+        nextD.textContent = "Submit";
+    }
+
+    if (index >= intro.length) {
+        const nameOfChar = txtField.value || "to lazy too input name";
+        storyField.innerHTML = `<p>Nice to meet you, ${nameOfChar}!</p>`;
+        nextD.textContent = "Next"
+    }
+});
+
+function sceneTwo (){
+
 }
-})
